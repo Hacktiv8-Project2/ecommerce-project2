0a96413ecc30a80910aaa646b1f27ef0984f5f91
ilyasmuhlisin <21ilyasmhlsn@gmail.com>
2023-05-18 19:53:34 +0700

    create checkout and recap page

--- src/features/cartSlice.js
+++ src/features/cartSlice.js
@@ -2,6 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   cart: [],
+  checkout: [],
 };
 
 const cartSlice = createSlice({
@@ -43,10 +44,36 @@ const cartSlice = createSlice({
           return item;
         });
     },
+
+    // updateStock: (state, action) => {
+    //   state.cart
+    //     ?.filter((item) => item.id === action.payload)
+    //     .map((item) => {
+    //       const currentStock = item.stock;
+    //       if (item.qty <= currentStock) {
+    //         item.stock = currentStock - item.qty;
+    //       } else {
+    //         item.stock = currentStock;
+    //       }
+    //       return item;
+    //     });
+    // },
+    clearCart: (state, action) => (state.cart = []),
+    setCheckout: (state, action) => {
+      state.checkout = action.payload;
+    },
   },
 });
 
-export const { addItem, removeItem, incrementItem, decrementItem } =
-  cartSlice.actions;
+export const {
+  addItem,
+  removeItem,
+  incrementItem,
+  decrementItem,
+  clearCart,
+  setCheckout,
+  // updateStock,
+} = cartSlice.actions;
 export const getAllCart = (state) => state.cart.cart;
+export const getAllCheck = (state) => state.cart.checkout;
 export default cartSlice.reducer;
--- src/pages/admin/RecapPages.jsx
+++ src/pages/admin/RecapPages.jsx
@@ -1,6 +1,10 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllCheck } from "../../features/cartSlice";
 
 function RecapPages() {
+  const dispatch = useDispatch();
+  const checkProduct = useSelector(getAllCheck);
   const data = [
     { column1: "Data 1", column2: "Data 2", column3: "Data 3" },
     { column1: "Data 4", column2: "Data 5", column3: "Data 6" },
@@ -10,22 +14,53 @@ function RecapPages() {
     <div className="overflow-x-auto">
       <table className="table-auto min-w-full">
         <thead>
-          <tr>
+          <tr className="bg-blue-500">
             <th className="px-4 py-2">product</th>
             <th className="px-4 py-2">Harga</th>
             <th className="px-4 py-2">Terjual</th>
+            <th className="px-4 py-2">Pendapatan</th>
           </tr>
         </thead>
         <tbody>
-          {data.map((row, index) => (
-            <tr key={index} className={index % 2 === 0 ? "bg-gray-100" : ""}>
-              <td className="border px-4 py-2">{row.column1}</td>
-              <td className="border px-4 py-2">{row.column2}</td>
-              <td className="border px-4 py-2">{row.column3}</td>
+          {checkProduct?.map((item, idx) => (
+            <tr key={idx} className={idx % 2 === 0 ? "bg-gray-100" : ""}>
+              <td className="border px-4 py-2">
+                <div className="flex items-center">
+                  <img
+                    className="w-16 h-16 object-cover mr-4"
+                    src={item.image}
+                    alt=""
+                  />
+                  <div className="px-6">
+                    <div className="font-medium text-lg mb-2">{item.title}</div>
+                    <div className="block text-md mb-2">{item.category}</div>
+                    <p className="text-gray-400 text-base">
+                      {item.description}
+                    </p>
+                  </div>
+                </div>
+              </td>
+              <td className="border px-4 py-2">
+                <div className="text-center">{item.price}</div>
+              </td>
+              <td className="border px-4 py-2">
+                <div className="text-center">{item.qty}</div>
+              </td>
+              <td className="border px-4 py-2">
+                <div className="text-center">{item.qty * item.price}</div>
+              </td>
             </tr>
           ))}
         </tbody>
       </table>
+      <div className="border text-end font-bold pl-2 pr-10 py-2 bg-blue-500">
+        Total Pendapatan :{" "}
+        {checkProduct
+          .reduce((sum, item) => {
+            return sum + item.qty * item.price;
+          }, 0)
+          .toFixed(2)}
+      </div>
     </div>
   );
 }
