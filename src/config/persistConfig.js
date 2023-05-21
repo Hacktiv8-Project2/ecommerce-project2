0ff50b14a7c4a2a6bce9745d68dbe39d90110bc9
ekialfani <ekialfani15@gmail.com>
2023-05-21 11:11:45 +0800

    fix bug: update stoke page data not refreshing after login.
    The previous bug caused the data on the update stoke page to disappear upon refreshing after login. This commit fixes the issue by implementing the necessary changes to ensure data persistence upon page refresh.

--- src/config/persistConfig.js
+++ src/config/persistConfig.js
@@ -3,7 +3,7 @@ import storage from 'redux-persist/lib/storage'
 const persistConfig = {
   key: "root",
   storage,
-  whitelist: ['auth', 'cart'] // Persisting only the 'auth' state to storage.
+  // whitelist: ['auth', 'cart'] // Persisting only the 'auth' state to storage.
 }
 
 export default persistConfig;
